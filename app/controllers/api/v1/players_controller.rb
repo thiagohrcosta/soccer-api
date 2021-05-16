33910efdf4c7b3487da013be6b8b43de29d26ad3
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-15 22:38:55 -0300

    Adds show action on controller with set_player

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
+  before_action :set_player, only: [ :show ]
+
   def index
     @restaurants = policy_scope(Restaurant)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_player
+    @player = Player.find(params[:id])
+    authorize @player
   end
 end
