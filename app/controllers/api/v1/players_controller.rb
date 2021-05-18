61eeb39f6a070bc5620b2610ea50c77f7223fbc3
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-17 22:54:45 -0300

    Completes the token implementation

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::PlayersController < Api::V1::BaseController
-  # acts_as_token_authentication_handler_for User, except: [:index, :show]
-  before_action :set_player, only: [:show]
+  acts_as_token_authentication_handler_for User, except: [:index, :show ]
+  before_action :set_player, only: [:show, :update]
 
   def index
     @players = policy_scope(Player)
@@ -9,10 +9,27 @@ class Api::V1::PlayersController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @player.update(player_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_player
     @player = Player.find(params[:id])
     authorize @player
+  end
+
+  def player_params
+    params.require(:player).permit(:name, :nationality, :club, :position, :market_value)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
